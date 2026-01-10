29c1f98a644e8df39e014ff0f8dac2405a3ceddb
Gregor Woiwode <gregor.woiwode@gmail.com>
2026-01-10 21:48:54 +0100

    fix: correct typo in TODO comment for function export instructions

--- modules/problem.js
+++ modules/problem.js
@@ -3,6 +3,6 @@ function greet(name) {
 }
 
 /** TODO:
- * - Exportiere die Funktion mit einem Named oder Defult Export
+ * - Exportiere die Funktion mit einem Named oder Default Export
  * - Verwende diese Funktion mittels import in einer client.js die du vorab erstellst
  */
