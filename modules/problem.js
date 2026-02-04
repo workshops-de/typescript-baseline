260a1650dabc1a116cc02548044a23c07a988e72
Gregor Woiwode <gregor.woiwode@gmail.com>
2026-02-04 11:16:21 +0100

    refactor: remove outdated problem and solution files for arrays, async/await, destructuring, and other topics to streamline the codebase

--- modules/problem.js
+++
@@ -1,8 +0,0 @@
-function greet(name) {
-  return `Hallo ${name}!`;
-}
-
-/** TODO:
- * - Exportiere die Funktion mit einem Named oder Default Export
- * - Verwende diese Funktion mittels import in einer client.js die du vorab erstellst
- */
